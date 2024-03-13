fe4aee4336d0af2ac30447d0535c5fc8f940ee31
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-03-13 20:34:00 +0100

    View creation & component creation

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,5 @@
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li><RouterLink to="/">Home</RouterLink></li>
-        <li><RouterLink to="/about">About</RouterLink></li>
-      </ul>
-    </nav>
-  </header>
-
   <RouterView />
 </template>
 
-<style scoped lang="scss">
-nav {
-  background-color: #f0f0f0;
-  padding: 1rem;
-
-  ul {
-    display: flex;
-    justify-content: center;
-    list-style: none;
-    padding: 0;
-
-    li {
-      margin: 0 1rem;
-
-      a {
-        text-decoration: none;
-        color: black;
-
-        &.router-link-active {
-          font-weight: bold;
-        }
-      }
-    }
-  }
-}
-</style>
+<style scoped lang="scss"></style>
