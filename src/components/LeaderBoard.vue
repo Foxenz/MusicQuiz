c310d396ec97fa4e55f52120ab8e606d60c83bf5
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-03-20 22:39:39 +0100

    Score Page + component LeaderBoard

---
+++ src/components/LeaderBoard.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="leaderboard">
+    <h2>Leaderboard</h2>
+    <ol>
+      <li v-for="(score, index) in scores" :key="index">
+        {{ score.name }}: {{ score.score }}
+      </li>
+    </ol>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LeaderBoard',
+
+  data() {
+    return {
+      scores: [
+        { name: 'John Doe', score: 25 },
+        { name: 'Jane Doe', score: 20 },
+        { name: 'John Smith', score: 15 },
+        { name: 'Jane Smith', score: 10 },
+        { name: 'John Johnson', score: 5 },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.leaderboard {
+  padding: 20px;
+  border-radius: 5px;
+  text-align: center;
+
+  h2 {
+    margin: 0 0 10px;
+    font-size: 1.5em;
+  }
+
+  ol {
+    padding: 0;
+    list-style: none;
+  }
+
+  li {
+    margin: 0 0 5px;
+    font-size: 1.2em;
+  }
+}
+</style>
--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,5 +1,80 @@
 <template>
-  <div class="result">
-    <h1>This is an result page</h1>
-  </div>
+  <main class="result">
+    <p>Result: {{ result }}</p>
+
+    <div>
+      <input type="text" placeholder="Enter your name" />
+      <button>Submit</button>
+    </div>
+
+    <LeaderBoard />
+
+    <router-link to="/" class="btn">Try Again</router-link>
+  </main>
 </template>
+
+<script>
+import LeaderBoard from '@/components/LeaderBoard.vue';
+
+export default {
+  data() {
+    return {
+      result: 25,
+    };
+  },
+
+  components: {
+    LeaderBoard,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.result {
+  margin: 20px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 300px;
+  text-align: center;
+
+  p {
+    margin: 0 0 10px;
+    font-size: 1.5em;
+  }
+
+  div {
+    input {
+      padding: 5px 2px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+
+    button {
+      padding: 5px 10px;
+      background-color: #007bff;
+      color: #fff;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #0056b3;
+      }
+    }
+  }
+
+  .btn {
+    display: inline-block;
+    padding: 5px 10px;
+    background-color: #007bff;
+    color: #fff;
+    text-decoration: none;
+    border-radius: 5px;
+
+    &:hover {
+      background-color: #0056b3;
+    }
+  }
+}
+</style>
