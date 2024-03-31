731df738fa57e6c626d1b96aeed43e59d1a2e354
Fred <frederic.vanovermeire@gmail.com>
2024-03-31 14:13:13 +0200

    quiz input working

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,31 +1,52 @@
 <template>
-  <div class="quiz">
-    <p>Quiz id: {{ id }}</p>
-    <TimerBar @time-is-up="stopQuestion" />
-  </div>
+    <div class="quiz">
+        <p>Quiz id: {{ id }}</p>
+        <TimerBar @time-is-up="stopQuestion"/>
+        <Answer
+            :questionContent="question.content"
+            :answer="question.answer"
+            @rightAnswer="handleRightAnswer"
+        />
+    </div>
 </template>
 
 <script>
 import TimerBar from '@/components/TimerBar.vue';
+import ApiHandler from "@/services/api/apiHandler.js";
+import Answer from "@/components/Answer.vue";
+
+const apiHandler = new ApiHandler();
 
 export default {
-  name: 'QuizView',
+    name: 'QuizView',
 
-  data() {
-    return {
-      id: this.$route.params.id,
-      timeIsUp: false,
-    };
-  },
-
-  methods: {
-    stopQuestion() {
-      console.log('Time is up!');
+    data() {
+        return {
+            id: this.$route.params.id,
+            timeIsUp: false,
+            question: {content: {}, answer: ''},
+            rightAnswer: false,
+        };
     },
-  },
 
-  components: {
-    TimerBar,
-  },
+    methods: {
+        stopQuestion() {
+            console.log('Time is up!');
+        },
+        handleRightAnswer(isCorrect) {
+            this.rightAnswer = isCorrect;
+        }
+    },
+
+    components: {
+        Answer,
+        TimerBar,
+    },
+
+    async created() {
+        const response = await apiHandler.fetchOneQuestionForCategory(2, 11);
+
+        this.question = response.questions[0];
+    }
 };
 </script>
