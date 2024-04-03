7ad71522553e7ea18de59789bd01540a0bea5830
Fred <frederic.vanovermeire@gmail.com>
2024-04-03 20:47:34 +0200

    add next question button

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,14 +1,13 @@
 <template>
-
-  <div class="quiz">
-    <p>Quiz id: {{ id }}</p>
-    <Question :question="question" />
-    <TimerBar @time-is-up="stopQuestion" />
-    <Answer :questionContent="question.content" :answer="question.answer" @rightAnswer="handleRightAnswer" />
-  </div>
-  <template v-if="rightAnswer !== null">
-    <button>Next</button>
-  </template>
+    <div class="quiz">
+        <p>Quiz id: {{ id }}</p>
+        <Question :question="question"/>
+        <TimerBar @time-is-up="stopQuestion"/>
+        <Answer :questionContent="question.content" :answer="question.answer" @rightAnswer="handleRightAnswer"/>
+    </div>
+    <button v-if="rightAnswer !== null && this.currentQuestion < this.questions.length - 1" @click="nextQuestion()">Next
+    </button>
+    <button v-if="rightAnswer !== null && this.currentQuestion === this.questions.length - 1" @click="gotToScore()">Show score</button>
 </template>
 
 <script>
@@ -19,47 +18,55 @@ import Answer from "@/components/Answer.vue";
 const apiHandler = new ApiHandler();
 
 export default {
-  name: 'QuizView',
+    name: 'QuizView',
 
-  data() {
-    return {
-      id: this.$route.params.id,
-      currentQuestion: 0,
-      timeIsUp: false,
-      questions: Array,
-      question: { content: {}, answer: '' },
-      rightAnswer: null,
-    };
-  },
-
-  methods: {
-    stopQuestion() {
-      console.log('Time is up!');
+    data() {
+        return {
+            id: this.$route.params.id,
+            currentQuestion: 0,
+            timeIsUp: false,
+            questions: Array,
+            question: {content: {}, answer: ''},
+            rightAnswer: null,
+        };
     },
-    handleRightAnswer(isCorrect) {
-      this.rightAnswer = isCorrect;
-    },
-    async fetchQuestionList() {
-      const res = await apiHandler.fetchQuestionsForCategory(this.id);
 
-      this.questions = res.questions
-    },
-    async fetchQuestion(questionID) {
-      const response = await apiHandler.fetchOneQuestionForCategory(this.id, questionID);
+    methods: {
+        stopQuestion() {
+            console.log('Time is up!');
+        },
+        handleRightAnswer(isCorrect) {
+            this.rightAnswer = isCorrect;
+        },
+        nextQuestion() {
+            this.currentQuestion++;
+            this.rightAnswer = null;
+            this.fetchQuestion(this.questions[this.currentQuestion].id)
+        },
+        gotToScore() {
+            this.$router.push(`/result`);
+        },
+        async fetchQuestionList() {
+            const res = await apiHandler.fetchQuestionsForCategory(this.id);
 
-      this.question = response.questions[0];
+            this.questions = res.questions
+        },
+        async fetchQuestion(questionID) {
+            const response = await apiHandler.fetchOneQuestionForCategory(this.id, questionID);
+
+            this.question = response.questions[0];
+        }
+    },
+
+    components: {
+        Answer,
+        TimerBar,
+    },
+
+    async created() {
+        await this.fetchQuestionList();
+        await this.fetchQuestion(this.questions[this.currentQuestion].id)
     }
-  },
-
-  components: {
-    Answer,
-    TimerBar,
-  },
-
-  async created() {
-    await this.fetchQuestionList();
-    await this.fetchQuestion(this.questions[this.currentQuestion].id)
-  }
 
 };
 </script>
