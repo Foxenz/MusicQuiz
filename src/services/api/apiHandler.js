76ea2204858e11b59af980e203aa2a11e1fe5e7d
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-04-17 21:55:57 +0200

    desactivate sortie d'album

--- src/services/api/apiHandler.js
+++ src/services/api/apiHandler.js
@@ -10,10 +10,12 @@ class ApiHandler {
   fetchCategories = async () => {
     const res = await this.axiosInstance('categories');
 
-    const response = this.checkResponse(res)
+    const response = this.checkResponse(res);
 
-    if(!response)
-      return null;
+    // Hardcode disable for category "Sortie d'album" id 3 set is_active to 0
+    response.data[2].is_active = 0;
+
+    if (!response) return null;
 
     return this.checkResponse(res).data;
   };
@@ -21,10 +23,9 @@ class ApiHandler {
   fetchQuestionsForCategory = async (categoryId) => {
     const res = await this.axiosInstance(`categories/${categoryId}`);
 
-    const response = this.checkResponse(res)
+    const response = this.checkResponse(res);
 
-    if(!response)
-      return null;
+    if (!response) return null;
 
     return this.checkResponse(res).data;
   };
@@ -34,34 +35,31 @@ class ApiHandler {
       `categories/${categoryId}?id=${questionID}`
     );
 
-    const response = this.checkResponse(res)
+    const response = this.checkResponse(res);
 
-    if(!response)
-      return null;
+    if (!response) return null;
 
     return this.checkResponse(res).data;
   };
 
   getIdCategoriesActivated = async () => {
-    const res = await this.axiosInstance('categories?is_active=1').catch(e => {
-      console.log("error: ", e)
-    });
+    const res = await this.axiosInstance('categories?is_active=1').catch(
+      (e) => {
+        console.log('error: ', e);
+      }
+    );
 
-    const response = this.checkResponse(res)
+    const response = this.checkResponse(res);
 
-    if(!response)
-      return null;
+    if (!response) return null;
 
     return this.checkResponse(res).data.map((category) => category.id);
-
   };
 
   checkResponse = (res) => {
-    if(res)
-      return res;
-    else
-      return null;
-  }
+    if (res) return res;
+    else return null;
+  };
 }
 
 export default ApiHandler;
